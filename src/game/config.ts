2ecabd917677c9ec69d2fc19627cb4ab52e9779d
王涛 <tao.wang.go@gmail.com>
2026-05-02 21:02:46 +0200

    Update config.ts

--- src/game/config.ts
+++ src/game/config.ts
@@ -6,6 +6,7 @@ class Config {
   public Scale: number = 1;
   public Orientation: Orientation = Orientation.Portrait;
   public Target: number = 10;
+  public TimeCount: number = 30;
 }
 
 export const config = new Config();
